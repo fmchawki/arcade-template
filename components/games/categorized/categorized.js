7f6b500b3e22f9a532f7f467ab20a51d343476df
Arikaim <sales@intersoft.biz>
2020-02-15 11:34:39 +0200

    =

--- components/games/categorized/categorized.js
+++ components/games/categorized/categorized.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     arikaim.ui.initImageLoader();
-    var category_id = $('#games_list').attr('category');
+    var categoryId = $('#games_list').attr('category');
 
     paginator.init('games_list',{
         name: 'arcade>games.categorized',
-        params: { category_id: category_id }
+        params: { category_id: categoryId }
     },'games.category'); 
 });
