967b075bf80cd17525d5faeaf7ee3160de1a62a9
Arikaim <sales@intersoft.biz>
2020-10-16 18:11:23 +0300

    =

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,14 +1,11 @@
 'use strict';
 
 $(document).ready(function() {
-    $('.category-dropdown').dropdown({});
-
     $('#language_dropdown').dropdown({
         onChange: function(value) {       
             arikaim.setLanguage(value);
         }
     });
-
     arikaim.ui.button('#open_sidebar',function(element) {       
         $('.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('show');       
     }); 
