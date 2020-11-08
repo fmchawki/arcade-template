cd1f74d7c6fe3888251cc701f79758137fc07518
Arikaim <sales@intersoft.biz>
2020-11-08 10:03:05 +0200

    =

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -7,6 +7,6 @@ $(document).ready(function() {
         }
     });
     arikaim.ui.button('#open_sidebar',function(element) {       
-        $('.sidebar').sidebar('setting', 'transition', 'overlay').sidebar('show');       
+        $('.sidebar').sidebar('setting','transition','overlay').sidebar('show');       
     }); 
 });
