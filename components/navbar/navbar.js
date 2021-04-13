065660b3fc8f5691a15cd5992b605ff809398f5f
Arikaim <sales@intersoft.biz>
2021-04-13 17:53:34 +0300

    =

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,11 +1,7 @@
 'use strict';
 
 arikaim.component.onLoaded(function() {
-    $('#language_dropdown').dropdown({
-        onChange: function(value) {       
-            arikaim.setLanguage(value);
-        }
-    });
+   
     arikaim.ui.button('#open_sidebar',function(element) {       
         $('.sidebar').sidebar('setting','transition','overlay').sidebar('show');       
     }); 
--- pages/user/__user.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-arikaim.component.onLoaded(function() {
-    arikaim.ui.tab('.user-menu-item','user_area_content');
-});
