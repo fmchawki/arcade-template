0b0910d9cc9896af45a90d5120dce82411dc5136
Arikaim <sales@intersoft.biz>
2021-05-06 11:41:54 +0300

    =

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,7 +1,6 @@
 'use strict';
 
 arikaim.component.onLoaded(function() {
-   
     arikaim.ui.button('#open_sidebar',function(element) {       
         $('.sidebar').sidebar('setting','transition','overlay').sidebar('show');       
     }); 
